34a69aee90862d98bddc432a9c19bc0cfc90bc8f
Abhishek.Kumar <abhishek.kumar@OPN-MC-341.local>
2024-05-15 13:31:24 +0530

    fixe assiment issue

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            primaryDark: '#0f1414',
+            primaryGray: '#1c1e1f',
+            paragraphGrey: '#8d8e8f',
+          },
+      },
+    },
+    plugins: [],
+  }
